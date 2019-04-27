3f59ce735b2449c6096267e35fef41870c04f01f
Ana Silva <ananunesdasilva@gmail.com>
2019-04-27 22:18:46 +0100

    Add jumbotron

--- src/app/nav/nav-bar.component.ts
+++ src/app/nav/nav-bar.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
-  templateUrl: './nav-bar.component.html'
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.scss']
 })
 
 export class NavBarComponent {
