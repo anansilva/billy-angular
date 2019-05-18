dc1f3f5e09bdfdc5dc16de69bbf4a4d674162d56
Ana Silva <ananunesdasilva@gmail.com>
2019-05-18 17:54:55 +0100

    Remove bootstrap, add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { ExpenseService } from './expenses/shared/expense.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [ExpenseService],
   bootstrap: [AppComponent]
