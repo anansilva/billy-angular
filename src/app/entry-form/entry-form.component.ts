3f679414cd839435fc1486231602f55238543388
Ana Silva <ananunesdasilva@gmail.com>
2019-05-20 21:31:09 +0100

    Validate amount (required)

--- src/app/entry-form/entry-form.component.ts
+++ src/app/entry-form/entry-form.component.ts
@@ -4,11 +4,15 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'entry-form',
-  templateUrl: './entry-form.component.html'
+  templateUrl: './entry-form.component.html',
+  styles: [`
+    .error {color: red; font-size: 12px;}
+  `]
 })
 export class EntryFormComponent {
   @Input()id: number;
-  myForm: FormGroup;
+  entryForm: FormGroup;  
+
   constructor(
    public activeModal: NgbActiveModal,
    private formBuilder: FormBuilder
@@ -16,12 +20,18 @@ export class EntryFormComponent {
     this.createForm();
   }
   private createForm() {
-    this.myForm = this.formBuilder.group({
-      amount: '',
-      description: ''
+    this.entryForm = this.formBuilder.group({
+      amount: ['', Validators.required],
+      description: ['', Validators.required],
+      date: [],
+      category: '',
     });
   }
+  public validateAmount() {
+    return this.entryForm.controls.amount.valid || this.entryForm.controls.amount.untouched;
+  }
+  
   private submitForm() {
-    this.activeModal.close(this.myForm.value);
+    this.activeModal.close(this.entryForm.value);
   }
 }
